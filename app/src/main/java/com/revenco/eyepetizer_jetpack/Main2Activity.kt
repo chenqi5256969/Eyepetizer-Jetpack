e30230c09e2e2137a949a42cecbd1ee5e9ee2cf3
陈启 <754003392@qq.com>
2020-05-06 11:19:05 +0800

    修改appbar的渐变样式

---
+++ app/src/main/java/com/revenco/eyepetizer_jetpack/Main2Activity.kt
@@ -0,0 +1,23 @@
+package com.revenco.eyepetizer_jetpack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.itemIconTintList = null
+        navView.labelVisibilityMode = LabelVisibilityMode.LABEL_VISIBILITY_LABELED
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+
+
+    }
+}
